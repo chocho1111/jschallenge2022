a2b2858d9889f80953bbeb1826ebf0541f361b7b
chocho1111 <goldpig070809@naver.com>
2022-02-20 19:53:07 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "36c500bb57907c66f21374dfb338ffcc";
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
